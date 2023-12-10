def1268f874c55da005cca6500f9bf255302d34f
LeandroNunes <35371333+LeandroNunesNascimento@users.noreply.github.com>
2023-12-09 23:38:31 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,27 @@
 import os
+import json
+import sys
+import boto3
 inputvariable = os.environ['INPUT_STORE']
 print(inputvariable)
 print('Hello World!')
+
+# Nested JSON data
+nested_data = {
+    'name': 'John',
+    'age': 30,
+    'children': [
+        {
+            'name': 'Alice',
+            'age': 5
+        },
+        {
+            'name': 'Bob',
+            'age': 7
+        }
+    ]
+}
+
+# Write nested JSON data to a file
+with open('nested_data.json', 'w') as f:
+    json.dump(nested_data, f)
