7791e67eef15a36143bf6f736871ae662c94085a
LeandroNunes <35371333+LeandroNunesNascimento@users.noreply.github.com>
2024-01-02 16:10:29 -0300

    x

--- chamada_api.py
+++ chamada_api.py
@@ -23,10 +23,10 @@ def chamada_api_func():
 
     print(url_put)
 
-    payload='{"name":"{sys.argv[1]}","enforcement":"active"}'
+    payload='{"name":"'{sys.argv[1]}'","enforcement":"active"}'
     print(payload)
 
-    response=''
+    response='
     result_expected='<Response [201]>'
     max_trying=3
     
