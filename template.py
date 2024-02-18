aecff964f9fc17b75009a555b081852cafb79ac2
LeandroNunes <35371333+LeandroNunesNascimento@users.noreply.github.com>
2024-02-18 17:02:04 -0300

    Create template.py

---
+++ template.py
@@ -0,0 +1,41 @@
+import sys
+import re
+import json
+import os
+import logging
+from datetime import  datetime
+import requests
+import shutil
+import subprocess as sp
+import time
+import urllib3
+
+urllib3.disable_warnings()
+
+def chamada_api_func():
+    github_url='https://api.github.com/repos/LeandroNunesNascimento/teste_info_template'
+
+    auth_token='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'
+
+
+    url_put=github_url
+
+    print(url_put)
+
+
+    response=''
+    result_expected='<Response [201]>'
+    max_trying=3
+    
+    
+    response = requests.get(url_put,
+            headers={'Content-Type':'application/json',
+                     'Authorization': f'Bearer {auth_token}'},
+                    verify=False)
+    logging.info(response)
+    logging.info(response.headers)
+    logging.info(response.content)
+    response_data = response.json()
+    print(response_data["template_repository"]["name"])
+
+chamada_api_func()
